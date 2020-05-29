b0bc44d958d7ae139b1c38c29322bdda06138384
Weiyu Huang <weiyu@apple.com>
2020-05-29 10:21:26 -0700

    View and search venue

--- fyyur/experiment.py
+++ fyyur/experiment.py
@@ -35,6 +35,8 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='venue', lazy=True)
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -47,14 +49,16 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='artist', lazy=True)
+
 
 class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, nullable=False, default=datetime)
-    artist = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
 def main():
     data = []
@@ -70,7 +74,7 @@ def main():
                      "venues": []})
 
     for venue in venues:
-        shows = Show.query.filter_by(venue=venue.id).all()
+        shows = Show.query.filter_by(venue_id=venue.id).all()
         num_upcoming_shows = len(shows)
 
         for venue_location in data:
